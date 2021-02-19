1f631a32a832d766b72ab212e0e99fdae59bb336
Caetano <caetano.see@gmail.com>
2021-02-19 11:47:59 -0500

    Added backup location for API

--- src/routes/articles/_articles.js
+++ src/routes/articles/_articles.js
@@ -4,9 +4,11 @@ import axios from "axios";
 let articles = [];
 let error = null;
 
+let url = process.env.API_URL || "https://the-link-cms-wnzzi.ondigitalocean.app/";
+
 onMount(async () => {
   try {
-    const res = await axios.get(process.env.API_URL + "/articles");
+    const res = await axios.get(url + "articles");
     articles = res.data;
   } catch (e) {
     error = e;
